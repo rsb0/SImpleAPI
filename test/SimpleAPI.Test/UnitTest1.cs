d4883b8d79fae1d7d56c48843b8b9e04c40e69df
rsb0 <rune.s.brekke@gmail.com>
2020-12-24 12:09:19 +0100

    first commit

---
+++ test/SimpleAPI.Test/UnitTest1.cs
@@ -0,0 +1,26 @@
+using System;
+using Xunit;
+using SimpleAPI.Controllers;
+
+namespace SimpleAPI.Test
+{
+    public class UnitTest1
+    {
+        ValuesController controller = new ValuesController();
+
+        /* Create new test. Typically name test method after 
+        what you expect a positive result to be */
+        [Fact]
+        public void GetReturnsMyName()
+        {
+            var returnValue = controller.Get(1);
+            Assert.Equal("Les Jackson", returnValue.Value);
+        }
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
