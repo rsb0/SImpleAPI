b89b55dd2ff8406e91047e3e9228c2d9b672626f
rsb0 <rune.s.brekke@gmail.com>
2021-02-22 18:34:40 +0100

    Updated yml file to run test

--- src/SimpleAPI/Controllers/ValuesController.cs
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -21,7 +21,7 @@ namespace SimpleAPI.Controllers
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "Rune Brekke";
+            return "Rune Breke";
         }
 
         // POST api/values
